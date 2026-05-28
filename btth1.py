163e462147bd159731c3d0d1cac943678e15def2
HO ANH HAO <haoanh802@gmail.coma>
2026-05-28 14:15:55 +0700

    btt1

---
+++ btth1.py
@@ -0,0 +1,15 @@
+total_amount = int(input("Nhập tổng số tiền hóa đơn ban đầu:  "))
+# aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+reduced_amount = 0
+
+if total_amount >= 500000 :
+    reduced_amount = total_amount * 0.1
+else :
+    reduced_amount = 0
+
+final_total = total_amount - reduced_amount
+
+print("---HÓA ĐƠN THANH TOÁN RIKKEI STORE----")
+print("Số tiền được giảm giá:",int(reduced_amount), "VND")
+print("Tổng tiền của khách phải trả:",int(final_total), "VND")
+
